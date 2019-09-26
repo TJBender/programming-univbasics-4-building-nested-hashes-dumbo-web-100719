6df67bec28c869e5e27532cba868c4e0f0470762
Tiana Bender <tiana.bender01@gmail.com>
2019-09-26 17:44:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,7 +11,13 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-    base_hash[:railroads] = :pieces
+   monopoly = {
+    railroads: {
+     pieces: 4,
+     names: {},
+     rent_in_dollars: {}
+   }
+  }
 end
 
 def monopoly_with_third_tier
