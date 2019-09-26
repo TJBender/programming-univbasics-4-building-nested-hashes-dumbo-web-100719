1cf6114647318088a306ce9db48e3856392b8c6e
Tiana Bender <tiana.bender01@gmail.com>
2019-09-26 17:51:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,6 @@ def monopoly_with_third_tier
         one_piece_owned:      25, 
         two_pieces_owned:     50,
         three_pieces_owned:   100, 
-# alt  :three_pieces_owned => 100, <===this will give you the same result 
         four_pieces_owned:    200
 # alt  :four_pieces_owned =>  200  <===this will give you the same result 
       }, 
